136dba1f561b980494a16d251a0b48a8fd7d1dcf
cko003 <cko003@bucknell.edu>
2023-09-24 09:48:18 -0400

    idle sprite animaation

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-const canvas = document.getElementByIdm("canvas1");
+const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+let x = 0;
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 3;
+let numberOfFrames = 6;
+let gameFrame = 0;
+const staggerFrame = 4;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  //   ctx.fillRect(50, 50, 100, 100);
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  if (gameFrame % staggerFrame == 0) {
+    if (frameX < numberOfFrames) frameX++;
+    else frameX = 0;
+  }
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+animate();
